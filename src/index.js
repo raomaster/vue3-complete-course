e1c0ead18a72824e92ee40d22be0ca3bc71810e5
Ricardo Oyarzo M <raomaster@gmail.com>
2021-08-13 19:09:28 -0400

    Bton que verifica si es par o no

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import * as Vue from 'vue/dist/vue.esm-bundler.js'
+import * as Vue from "vue/dist/vue.esm-bundler.js";
 
 const app = Vue.createApp({
-    template: `
-     <button v-on:click="increment(5)">Increment</button>
-     <p>{{ count }}</p>
+  template: `
+        <button v-on:click="increment(5)">Increment</button>
+        <p>{{ count }}</p>
+        <div v-if="isEven(count)">
+            Even
+        </div>
+        <div v-else>
+            Odd
+        </div>
     `,
-    data() {
-        return {
-            count: 0
-        }
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment(val) {
+      this.count += val;
     },
-    methods: {
-        increment(val) {
-            this.count += val
-        }
+    isEven() {
+        return this.count % 2 === 0
     }
-})
-app.mount('#app')
+  },
+});
+app.mount("#app");
