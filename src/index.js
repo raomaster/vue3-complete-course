e5efc32eab8f146a546d756d275419973c23a8f9
Ricardo Oyarzo M <raomaster@gmail.com>
2021-08-13 20:39:32 -0400

    components Chiled and Parent communication

--- src/index.js
+++ src/index.js
@@ -1,76 +1,90 @@
 import * as Vue from "vue/dist/vue.esm-bundler.js";
 
+const Num = {
+  props: ["number"],
+  template: `
+    <button 
+        v-bind:class="getClass(number)"
+        v-on:click="click"    
+    >
+        {{ number }}
+        <span v-if="isEven(number)">
+            Even
+        </span>
+        <span v-else>
+            Odd
+        </span>    
+    </button>
+    `,
+  methods: {
+    click() {
+        this.$emit('chosen', this.number)
+        console.log(this.number)
+    },
+    isEven(number) {
+      return number % 2 === 0;
+    },
+    getClass(number) {
+      return this.isEven(number) ? "blue" : "red";
+    },
+  },
+};
+
 const app = Vue.createApp({
+  components: {
+    Num,
+  },
+
   template: `
         <button v-on:click="increment(5)">Increment</button>
         <p>{{ count }}</p>
 
         <input 
-            v-bind:value="value" 
-            v-on:input="input"
+            type="text"
+            v-model="value"
         />
         <div class="red">
             {{ error }}
         </div>
-        <div 
-            v-for="number in numbers" 
-            v-bind:class="getClass(number)"
-        >
-            <div> {{ number }} 
-                <span v-if="isEven(number)">
-                    Even
-                </span>
-                <span v-else>
-                    Odd
-                </span>    
-            </div>
-        </div>
 
-        <div v-if="isEven(count)">
-            Even
-        </div>
-        <div v-else>
-            Odd
-        </div>
+
+        <num 
+            v-for="number in numbers" 
+            v-bind:number="number"
+            v-on:chosen="addNumber" 
+        /> 
+        <hr />
+        <num 
+            v-for="number in numberHistory" 
+            v-bind:number="number"
+        /> 
     `,
   data() {
     return {
       count: 0,
-      numbers: [1,2,3,4,5,6,7,8,9,10],
-      value: 'user',
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      value: "user",
+      numberHistory: []
     };
   },
 
   computed: {
     evenList() {
-        return this.numbers.filter(
-            num=> this.isEven(num)
-        )
+      return this.numbers.filter((num) => this.isEven(num));
     },
     error() {
-        if (this.value.length <5) {
-            return "Must be greater than 5"
-  
-        }
-    }
+      if (this.value.length < 5) {
+        return "Must be greater than 5";
+      }
+    },
   },
   methods: {
     increment(val) {
       this.count += val;
     },
-    isEven(number) {
-        return number % 2 === 0
-    },
-    getClass(number) {
-        return this.isEven(number) ? 'blue' : 'red'
-    },
-    input($event) {
-        this.value = $event.target.value
-        if (this.value.length <5) {
-            this.error = "Must be greater than 5"
-        } else {
-            this.error = ''
-        }
+    addNumber(number) {
+        console.log('number: ', number)
+        this.numberHistory.push(number)
     }
   },
 });
