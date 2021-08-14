57f6e2c7a647fd8f1c99093c23bc681ed0016ed6
Ricardo Oyarzo M <raomaster@gmail.com>
2021-08-14 01:09:14 -0400

    composition API: getting started

---
+++ composition/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <button @click="increment" class="">
+    {{ count }}
+
+  </button>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0)
+    const increment = () => {
+      count.value++
+    }
+    return {
+      count,
+      increment
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  height: 200px;
+  width: 200px;
+  font-size: 40px;
+}
+</style>
